6a0d15e8df6a98eb71a1bc44cd7e8850a38553b7
huangwyn <91707274+huangwyn@users.noreply.github.com>
2022-09-28 19:14:33 -0400

    Completion of Circuit Breaker Demo

--- reading/src/main/java/hello/ReadingApplication.java
+++ reading/src/main/java/hello/ReadingApplication.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Mono;
 
 import org.springframework.boot.SpringApplication;
@@ -12,11 +13,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 @SpringBootApplication
 public class ReadingApplication {
 
+  @Autowired
+  private BookService bookService;
+
   @RequestMapping("/to-read")
   public Mono<String> toRead() {
-    return WebClient.builder().build()
-        .get().uri("http://localhost:8090/recommended").retrieve()
-        .bodyToMono(String.class);
+    return bookService.readingList();
   }
 
   public static void main(String[] args) {
